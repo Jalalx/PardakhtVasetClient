be47aa260aea57f3e4df21d1a08eeb34747305de
jalalx <sjamini@gmail.com>
2018-02-28 15:04:06 +0330

    new tests added

---
+++ src/PardakhtVasetClientTests/PaymentLinkNotificationServiceTests.cs
@@ -0,0 +1,149 @@
+﻿using Moq;
+using PardakhtVasetServices;
+using Septa.PardakhtVaset.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PardakhtVasetClientTests
+{
+    public class PaymentLinkNotificationServiceTests
+    {
+        [Fact]
+        public void OnTick_WithPaidLinkAndHandlingPaymentLinkEvent_WorksAsExpected()
+        {
+            var options = new PardakhtVasetClientOptions();
+            options.ApiKey = "foo";
+            options.ConnectionString = "fake connection string";
+            options.TablePrefix = "";
+
+            var mockedRepository = new Mock<IPaymentLinkRepository>();
+            var mockedPayRequest = new Mock<IPayRequest>();
+
+            var nextPaymentLink = new PaymentLink();
+            nextPaymentLink.Amount = 26000;
+            nextPaymentLink.BankReferenceId =null;
+            nextPaymentLink.CreateDate = new DateTime(2017, 12, 3);
+            nextPaymentLink.Description = "بابت مانده حساب";
+            nextPaymentLink.ExpireDays = 30;
+            nextPaymentLink.FollowId = "324";
+            nextPaymentLink.LastCheckForUpdateDate = new DateTime(2017, 12, 3);
+            nextPaymentLink.PaymentStatus = (int)RequestStatus.Initiated;
+            nextPaymentLink.ResultDate = null; 
+            nextPaymentLink.Token = "1d372049-17de-4550-aecc-06111fdf2a9b";
+            nextPaymentLink.Url = "https://pardakhtvaset.com/pay?token=1d372049-17de-4550-aecc-06111fdf2a9b";
+
+            mockedRepository.Setup(x => x.GetNextLinkForCheck()).Returns(nextPaymentLink);
+
+            var fakeEpayResult = new EPayRequestCheckResult();
+            fakeEpayResult.BankReferenceId = "57430753503405340";
+            fakeEpayResult.RequestStatus = RequestStatus.Paid;
+            fakeEpayResult.Success = true;
+            fakeEpayResult.VerifyDate = new DateTime(2017, 12, 4);
+            mockedPayRequest.Setup(x => x.Check(options.ApiKey, nextPaymentLink.Token)).Returns(fakeEpayResult);
+            
+            var service = new PaymentLinkNotificationService(options, mockedRepository.Object, mockedPayRequest.Object);
+            service.PaymentLinkChanged += (o, e) => e.Handled = true;
+
+            service.OnTick();
+
+            mockedRepository.Verify(x => x.GetNextLinkForCheck(), Times.Once());
+            mockedPayRequest.Verify(x => x.Check(options.ApiKey, nextPaymentLink.Token), Times.Once());
+            mockedRepository.Verify(x => x.Update(nextPaymentLink), Times.Once());
+        }
+
+        [Fact]
+        public void OnTick_WithPaidLinkAndNotHandlingPaymentLinkEvent_NotCallingUpdate()
+        {
+            var options = new PardakhtVasetClientOptions();
+            options.ApiKey = "foo";
+            options.ConnectionString = "fake connection string";
+            options.TablePrefix = "";
+
+            var mockedRepository = new Mock<IPaymentLinkRepository>();
+            var mockedPayRequest = new Mock<IPayRequest>();
+
+            var nextPaymentLink = new PaymentLink();
+            nextPaymentLink.Amount = 26000;
+            nextPaymentLink.BankReferenceId = null;
+            nextPaymentLink.CreateDate = new DateTime(2017, 12, 3);
+            nextPaymentLink.Description = "بابت مانده حساب";
+            nextPaymentLink.ExpireDays = 30;
+            nextPaymentLink.FollowId = "324";
+            nextPaymentLink.LastCheckForUpdateDate = new DateTime(2017, 12, 3);
+            nextPaymentLink.PaymentStatus = (int)RequestStatus.Initiated;
+            nextPaymentLink.ResultDate = null;
+            nextPaymentLink.Token = "1d372049-17de-4550-aecc-06111fdf2a9b";
+            nextPaymentLink.Url = "https://pardakhtvaset.com/pay?token=1d372049-17de-4550-aecc-06111fdf2a9b";
+
+            mockedRepository.Setup(x => x.GetNextLinkForCheck()).Returns(nextPaymentLink);
+
+            var fakeEpayResult = new EPayRequestCheckResult();
+            fakeEpayResult.BankReferenceId = "57430753503405340";
+            fakeEpayResult.RequestStatus = RequestStatus.Paid;
+            fakeEpayResult.Success = true;
+            fakeEpayResult.VerifyDate = new DateTime(2017, 12, 4);
+            mockedPayRequest.Setup(x => x.Check(options.ApiKey, nextPaymentLink.Token)).Returns(fakeEpayResult);
+
+            var service = new PaymentLinkNotificationService(options, mockedRepository.Object, mockedPayRequest.Object);
+            service.PaymentLinkChanged += (o, e) => e.Handled = false;
+
+            service.OnTick();
+
+            mockedRepository.Verify(x => x.GetNextLinkForCheck(), Times.Once());
+            mockedPayRequest.Verify(x => x.Check(options.ApiKey, nextPaymentLink.Token), Times.Once());
+            mockedRepository.Verify(x => x.Update(nextPaymentLink), Times.Never());
+        }
+
+        [Fact]
+        public void OnTick_CheckResultFailsForSomeReason_NotCallingEventNorUpdate()
+        {
+            var paymentLinkEventCalled = false;
+            var options = new PardakhtVasetClientOptions();
+            options.ApiKey = "foo";
+            options.ConnectionString = "fake connection string";
+            options.TablePrefix = "";
+
+            var mockedRepository = new Mock<IPaymentLinkRepository>();
+            var mockedPayRequest = new Mock<IPayRequest>();
+
+            var nextPaymentLink = new PaymentLink();
+            nextPaymentLink.Amount = 26000;
+            nextPaymentLink.BankReferenceId = null;
+            nextPaymentLink.CreateDate = new DateTime(2017, 12, 3);
+            nextPaymentLink.Description = "بابت مانده حساب";
+            nextPaymentLink.ExpireDays = 30;
+            nextPaymentLink.FollowId = "324";
+            nextPaymentLink.LastCheckForUpdateDate = new DateTime(2017, 12, 3);
+            nextPaymentLink.PaymentStatus = (int)RequestStatus.Initiated;
+            nextPaymentLink.ResultDate = null;
+            nextPaymentLink.Token = "1d372049-17de-4550-aecc-06111fdf2a9b";
+            nextPaymentLink.Url = "https://pardakhtvaset.com/pay?token=1d372049-17de-4550-aecc-06111fdf2a9b";
+
+            mockedRepository.Setup(x => x.GetNextLinkForCheck()).Returns(nextPaymentLink);
+
+            var fakeEpayResult = new EPayRequestCheckResult();
+            fakeEpayResult.BankReferenceId = null;
+            fakeEpayResult.RequestStatus = RequestStatus.Initiated;
+            fakeEpayResult.ExceptionType = ExceptionType.ArgumentException;
+            fakeEpayResult.Message = "some random error message";
+            fakeEpayResult.Success = false;
+            fakeEpayResult.VerifyDate = null;
+            mockedPayRequest.Setup(x => x.Check(options.ApiKey, nextPaymentLink.Token)).Returns(fakeEpayResult);
+
+            var service = new PaymentLinkNotificationService(options, mockedRepository.Object, mockedPayRequest.Object);
+            service.PaymentLinkChanged += (o, e) => paymentLinkEventCalled = true;
+
+            service.OnTick();
+
+            mockedRepository.Verify(x => x.GetNextLinkForCheck(), Times.Once());
+            mockedPayRequest.Verify(x => x.Check(options.ApiKey, nextPaymentLink.Token), Times.Once());
+            mockedRepository.Verify(x => x.Update(nextPaymentLink), Times.Never());
+            Assert.False(paymentLinkEventCalled);
+        }
+
+    }
+}
